9504046d235429919ca8dfbb8d588c16329e3de1
Julie Smyth <juliemcsmyth@gmail.com>
2020-07-01 16:14:04 -0400

    tiny adjustments

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -5,13 +5,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class MoviesService {
-  // data: any = [
-  //   { temp: 90, weather: "sunny", date: "20-05-20" },
-  //   { temp: 80, weather: "cloudy", date: "20-05-21" },
-  //   { temp: 70, weather: "rainy", date: "20-05-22" },
-  //   { temp: 75, weather: "clear", date: "20-05-23" },
-  //   { temp: 22, weather: "snow", date: "20-05-24" },
-  // ]
 
   apiKey: string = '9de00a3aded0074e4a583ad4a86ef37b';
   baseUrl: string = 'https://api.themoviedb.org/3';
@@ -34,20 +27,15 @@ export class MoviesService {
   //add in methods here
 
   getGenreData() {
-    // return this.data;\
+    // return this.data
     return this.http.get(this.genreEndpoint, {
       params: {
-        //params must match how it is written in the API. Refer to how the API calls each parameter (such as api_key)
+
         api_key: this.apiKey,
       },
     });
   }
-  //add in all of the parameters for the different things here
-  //refer to the example with Kyle where we used an api and put in city and units
-  //we need to replace title with a different filter
-  //getMovieData will get this info from the form
-  //on ngsubmit ( so first connect this to the search-criteria component then the html )
-  //for the picture find where it is coming from on PostMan first
+
   getDiscoverData(year: string, vote: string, genre: string) {
     console.log(year, vote, genre);
     return this.http.get(this.discoverEndpoint, {
